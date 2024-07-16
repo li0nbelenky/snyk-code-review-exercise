22aa9e6b23dc4c1d6e8bd0088181d4a0026b7e1d
li0nbelenky <arie.belenky@checkmarx.com>
2024-07-16 12:02:43 +0300

    save progress

--- src/package.ts
+++ src/package.ts
@@ -2,26 +2,45 @@ import { RequestHandler } from 'express';
 import { maxSatisfying } from 'semver';
 import got from 'got';
 import { NPMPackage } from './types';
+import pLimit = require('p-limit') ;
 
-type Package = { version: string; dependencies: Record<string, Package> };
+const limit = pLimit(100);
+
+type Package = { name?: string, version: string; dependencies: Record<string, Package> };
 
 /**
  * Attempts to retrieve package data from the npm registry and return it
  */
+const npmUrl = (name: string) => {
+  return `https://registry.npmjs.org/${name}`;
+}
+
 export const getPackage: RequestHandler = async function (req, res, next) {
   const { name, version } = req.params;
   const dependencyTree = {};
+  
   try {
     const npmPackage: NPMPackage = await got(
-      `https://registry.npmjs.org/${name}`,
+      npmUrl(name),
     ).json();
 
     const dependencies: Record<string, string> =
       npmPackage.versions[version].dependencies ?? {};
+    const tasks :any = [];
     for (const [name, range] of Object.entries(dependencies)) {
-      const subDep = await getDependencies(name, range);
-      dependencyTree[name] = subDep;
+      tasks.push(await limit(async ()=>{
+        const response = await getDependencies(name, range);
+        response.name = name;
+        return response;
+      }));
+      // const subDep = await getDependencies(name, range);
+      // dependencyTree[name] = subDep;
     }
+
+    const promisifedDeps = await Promise.all(tasks);
+    for (const depRecord of promisifedDeps){
+      dependencyTree[depRecord.name as string] = depRecord;
+    };
 
     return res
       .status(200)
@@ -33,7 +52,7 @@ export const getPackage: RequestHandler = async function (req, res, next) {
 
 async function getDependencies(name: string, range: string): Promise<Package> {
   const npmPackage: NPMPackage = await got(
-    `https://registry.npmjs.org/${name}`,
+    npmUrl(name),
   ).json();
 
   const v = maxSatisfying(Object.keys(npmPackage.versions), range);
